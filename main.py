5300108873da1fb672efe18b6da600f2bc199dc7
Dominik Cydzik <dominikcydzik@85-195-198-38.init7.net>
2023-06-03 20:05:37 +0200

    added category asigner

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import difflib
 
 
 def read_csv(path):
@@ -22,6 +23,62 @@ def read_csv(path):
     del df2['Truncated Description']
 
     return df2
+
+def process_data(data):
+
+    classes = [
+                "Food",
+                "Gifts",
+                "Health/medical",
+                "Home",
+                "Transportation",
+                "Personal",
+                "Utilities",
+                "Travel",
+                "Sports/Fitness",
+                "Other",
+                "Hobbies",
+                "Investments",
+                "Entertainment"
+                ]
+    
+    df = data
+
+    # add category column to populate
+    df['Category'] = pd.Series(dtype=str)
+
+    # iterate through transaction rows to asign them categories
+    for index, row in df.iterrows():
+
+        print(row)
+        x = input("Category: ")
+
+
+        x_a = difflib.get_close_matches(x, classes, 1, 0.3)[0]
+
+
+        row['Category'] = x_a
+
+        df.at[index, "Category"] = x_a
+
+    ## TO DO
+    # print the updated transactions with the categories and ask for confirmation
+    # allow to toggle between potential options --> CONFIRM, EDIT, etc.
+
+
+    df_out = df
+
+    return df_out
+
+
+
+
+
+
+
+
+
+
     
 
 
@@ -29,4 +86,4 @@ path = 'data/historia_2023-05-16_04109025900000000153544523.csv'
 
 data = read_csv(path)
 
-print(data)
+df_out = process_data(data)
