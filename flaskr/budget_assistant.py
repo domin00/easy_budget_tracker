3ca3eb631e4384f05c805971d467467bb57828f5
Dominik Cydzik <dominik.cydzik@gmail.com>
2023-11-15 13:24:08 +0100

    improving budget assistant

--- flaskr/budget_assistant.py
+++ flaskr/budget_assistant.py
@@ -5,27 +5,72 @@ from werkzeug.exceptions import abort
 
 from flaskr.auth import login_required
 from flaskr.db import get_db
+import json
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
 
 from scripts.database import *
 
 bp = Blueprint('budget_assistant', __name__)
+
+# Category label map
+with open('data/categories.json', 'r') as file:
+    supported_categories = json.load(file)
+CATEGORY_MAP = {index+1: category for index, category in enumerate(supported_categories)}
 
 
 @bp.route('/transactions')
 def index():
     transactions = get_all_transactions()
     
-    return render_template('budget_assistant/index.html', transactions=transactions)
+    return render_template('budget_assistant/transactions.html', transactions=transactions)
 
 @bp.route('/summary', methods=('GET', 'POST'))
 @login_required
 def summary():
     transactions = get_all_transactions()
+
+    summary_table, totals = process_transactions(transactions)
+
+    # plot_url = generate_line_plot(summary_table)
+    
+    # Pass the summary_table to the template
+    return render_template('budget_assistant/summary.html', summary_table=summary_table, totals=totals)#, plot_url=plot_url)
+
+def process_transactions(transactions):
+
     transactions['Month'] = pd.to_datetime(transactions['Date']).dt.strftime('%Y-%m')
+    transactions['Category'] = transactions['CategoryID'].map(CATEGORY_MAP)
+    # transactions['Amount'] = round(transactions['Amount'], 2)
     
     # Group transactions by Month and CategoryID, summing up Amount
     # Create a summary table with CategoryID as rows, Month as columns, and sum of Amount as values
-    summary_table = pd.pivot_table(transactions, values='Amount', index='CategoryID', columns='Month', aggfunc='sum', fill_value=0)
-    totals = summary_table.sum(axis=0)
-    # Pass the summary_table to the template
-    return render_template('budget_assistant/summary.html', summary_table=summary_table, totals=totals)
+    summary_table = pd.pivot_table(transactions, values='Amount', index='Category', columns='Month', aggfunc='sum', fill_value=0)
+    summary_table = round(summary_table, 2)
+    totals = round(summary_table.sum(axis=0), 2)
+
+    return summary_table, totals
+
+# Function to generate a line plot and return it as base64 encoded image
+def generate_line_plot(summary_table):
+    plt.figure(figsize=(10, 6))
+    for category in summary_table.index:
+        plt.plot(summary_table.columns, summary_table.loc[category], label=category)
+
+    plt.title('Expenses per Category')
+    plt.xlabel('Month')
+    plt.ylabel('Expense Amount')
+    plt.legend()
+    plt.grid(True)
+    
+    # Save plot to BytesIO
+    img = BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    
+    # Encode plot image to base64
+    plot_url = base64.b64encode(img.getvalue()).decode('utf8')
+    plt.close()
+    
+    return plot_url
