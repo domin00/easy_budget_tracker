d557d34a666d3afdb6c6f73bb5298a80517830e8
domin00 <dominik.cydzik@gmail.com>
2024-02-13 14:52:59 +0100

    implemented streamlit functionality - amazing results!!!

---
+++ app.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime
+from scripts import csv_processor
+
+# Function to process CSV based on date range and display transactions
+def process_csv(file, bank, start_date, end_date):
+    df = csv_processor.parse_csv(file, bank)
+    
+    # Filter transactions based on date range
+    mask = (df['Date'] >= start_date) & (df['Date'] <= end_date)
+    filtered_df = df[mask]
+    
+    return filtered_df
+
+# Function to filter transactions based on category
+def filter_by_category(df, category):
+    if category == 'All':
+        return df
+    return df[df['Category'] == category]
+
+# Streamlit App
+def main():
+    st.title("CSV Transactions Processor")
+
+    # Upload CSV file
+    uploaded_file = st.file_uploader("Upload CSV file", type=["csv"])
+    
+
+    if uploaded_file is not None:
+        # Date range selection
+        start_date = st.date_input("Select start date", min_value=datetime.min, max_value=datetime.max)
+        end_date = st.date_input("Select end date", min_value=datetime.min, max_value=datetime.max)
+
+        # Bank selection
+        bank_options = ['Revolut', 'UBS', 'UBS Main', 'Santander']
+        bank = st.selectbox("Select bank", bank_options)
+
+        # Process CSV and display transactions
+        transactions_df = process_csv(uploaded_file, bank, start_date, end_date)
+
+        # Display transactions table
+        st.subheader("Transactions")
+        st.dataframe(transactions_df, width=800)
+
+        # # Category filter
+        # categories = transactions_df['Category'].unique().tolist()
+        # categories.insert(0, 'All')
+
+        # selected_category = st.selectbox("Select category", categories)
+
+        # # Filter transactions by category
+        # filtered_df = filter_by_category(transactions_df, selected_category)
+
+        # # Display filtered transactions
+        # st.subheader("Filtered Transactions")
+        # st.write(filtered_df)
+
+if __name__ == "__main__":
+    main()
